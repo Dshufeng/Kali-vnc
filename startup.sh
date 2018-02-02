47c5c8615ed5f2bc25c96718129f4c073054f7bd
shufengDong <839117348@qq.com>
2018-02-02 10:18:00 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 rm -rfv /tmp/.X*-lock /tmp/.X11-unix || echo "remove old vnc locks to be a reattachable container"
-# Start XServer
-vncserver :1 &
+
 
 ## change vnc password
 echo -e "\n------------------ change VNC password  ------------------"
 # first entry is control, second is view (if only one is valid for both)
 mkdir -p "$HOME/.vnc"
 PASSWD_PATH="$HOME/.vnc/passwd"
-echo "$VNC_PW" | vncpasswd -f >> $PASSWD_PATH
+echo 123456 | vncpasswd -f >> $PASSWD_PATH
 chmod 600 $PASSWD_PATH
+
+# Start XServer
+vncserver :1 &
 
 tail -f /dev/null
 
