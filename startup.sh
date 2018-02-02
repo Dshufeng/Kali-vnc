d6a7b48cd06ee27d58d23fb46db825223eac0a34
shufengDong <839117348@qq.com>
2018-02-02 10:04:54 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,5 +3,13 @@ rm -rfv /tmp/.X*-lock /tmp/.X11-unix || echo "remove old vnc locks to be a reatt
 # Start XServer
 vncserver :1 &
 
+## change vnc password
+echo -e "\n------------------ change VNC password  ------------------"
+# first entry is control, second is view (if only one is valid for both)
+mkdir -p "$HOME/.vnc"
+PASSWD_PATH="$HOME/.vnc/passwd"
+echo "$VNC_PW" | vncpasswd -f >> $PASSWD_PATH
+chmod 600 $PASSWD_PATH
+
 tail -f /dev/null
 
